b57e7129567822e85482f124211f3d44e94464c5
Leperlier Nathaniel <nathaniel.leperlier@epitech.eu>
2025-04-06 10:11:07 +0400

    teste rangement js

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMenu() {
+    const menu = document.getElementById('mobile-menu');
+    menu.classList.toggle('hidden');
+  }
+    AOS.init({ duration: 800 });
+    let lastScrollTop = 0;
+    const header = document.querySelector('header');
+  
+window.addEventListener('scroll', () => {
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop) {
+        header.style.transform = 'translateY(-100%)';
+    } else {
+        header.style.transform = 'translateY(0)';
+    }
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+    });
